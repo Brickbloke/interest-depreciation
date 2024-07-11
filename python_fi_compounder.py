e30207e6cacce2c21e283e370cf5db282c55e629
Brickbloke <106596322+Brickbloke@users.noreply.github.com>
2024-07-12 00:09:05 +0400

    Update python_fi_com.py
    asked for input

--- python_fi_compounder.py
+++ python_fi_compounder.py
@@ -1,6 +1,20 @@
 while True:
   try:
     user_loss_input = input("have you lost money this year? (type = yes or no)")
+    user_loss_input =
     if user_loss_input = "yes"
-        
+        print(" we are sad to hear you lost money ")
+        PV = input("numbers insert your intial investment")
+        r = input("insert the rate of depreciation")
+        n = input("please insert the number of year you had invested for")
+    elif user_loss_input = "no":
+        print("congragulations we will need some information to calculate your compound interest")
+        PV = input("numbers insert your intial investment")
+        r = input("insert the rate of depreciation")
+        n = input("please insert the number of year you had invested for")    
+  else:
+        continue
+    
+
+   
        
