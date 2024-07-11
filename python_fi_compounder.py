cc2485e74bfd4ede08c1c976dc0e2669c6005c2d
Brickbloke <106596322+Brickbloke@users.noreply.github.com>
2024-07-12 00:25:24 +0400

    Update python_fi_compounder.py
    minor fixes

--- python_fi_compounder.py
+++ python_fi_compounder.py
@@ -2,7 +2,7 @@ while True:
   try:
     user_loss_input = input("have you lost money this year? (type = yes or no)")
     user_loss_input =
-    if user_loss_input = "yes"
+    if user_loss_input == "yes"
         print(" we are sad to hear you lost money ") 
         Pv = input("numbers insert your intial investment") #intitial cash investment
         Pv = float(Pv)
@@ -11,9 +11,9 @@ while True:
         n = input("please insert the number of year you had invested for") #years
         n = float(n)
 
-        FVL = Pv * (1 - ( r / 100 )^ n
+        FVL = Pv * (1 - ( r / 100 ) ** n
                     
-    elif user_loss_input = "no":
+    elif user_loss_input == "no":
         print("congragulations we will need some information to calculate your compound interest")
         cv = input("numbers insert your intial investment") #initial cash investment
         cv = float(cv)
@@ -24,9 +24,9 @@ while True:
         cp = input("compounding periods for your investment") #compounding times
         cp = float(cp)
     
-        FV = cv * (1 + ( i / ( 100 * cp ) )^( cp * ny )
+        FV = cv * (1 + ( i / ( 100 * cp ) ) ** ( cp * ny )
         
-  else:
+    else:
         continue
     
 
